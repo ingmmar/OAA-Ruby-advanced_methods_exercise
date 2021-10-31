9cc7057d2db5246a16ee9a34340a133c7c6f6609
ingmmar <81257110+ingmmar@users.noreply.github.com>
2021-10-31 18:10:30 +0100

    Add files via upload

---
+++ 01_coprime.rb
@@ -0,0 +1,25 @@
+# Write a method, coprime?(num_1, num_2), that accepts two numbers as args.
+# The method should return true if the only common divisor between the two numbers is 1.
+# The method should return false otherwise. For example coprime?(25, 12) is true because
+# 1 is the only number that divides both 25 and 12.
+def coprime?(num1,num2)
+    if num1 > num2
+        big_n = num1
+        sml_n = num2
+    else
+        big_n = num2
+        sml_n = num1
+    end
+
+    for i in 2...sml_n
+        if sml_n%i == 0 and big_n%i == 0
+            return false
+        end
+    end
+    return true
+end
+
+p coprime?(25, 12)    # => true
+p coprime?(7, 11)     # => true
+p coprime?(30, 9)     # => false
+p coprime?(6, 24)     # => false
